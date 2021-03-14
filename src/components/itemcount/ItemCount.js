4ce399668b12d3f3fffb17c2c342d05e3b291985
Your Name <you@example.com>
2021-03-14 15:15:42 -0300

    ItemCount Mejorado

--- src/components/itemcount/ItemCount.js
+++ src/components/itemcount/ItemCount.js
@@ -1,20 +1,27 @@
-import { useState } from "react";
-import { Button, ButtonGroup, Card, InputGroup } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, ButtonGroup, Card, Form, InputGroup } from "react-bootstrap";
+import RangeSlider from "react-bootstrap-range-slider";
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 
 
 const ItemCount = ({stock, initial, onAdd}) =>{
     
     const [contador, setContador] = useState(initial);
-        
-    const handleOnAdd = (e)=>{
+    const [variant, setVariant] = useState("info")
+    
+    const handleCounter = (e)=>{
         
         if ((e.target.value === "-") && (initial < contador)) {
-            setContador((contador)=>(contador - 1 ));
+            setContador(contador=>(contador=contador-1));
         }
         if ((e.target.value === "+") && (stock > contador)){
-            setContador((contador)=>(contador + 1 ));
+            setContador(contador=>(contador=parseInt(contador)+1) );
         }
-        
+        if ((e.target.type === "range") && (stock >= contador)){
+            setContador(e.target.value);
+        }
+        console.log(contador);
+                
     }    
 
     const handleOnSubmit = (e)=>{
@@ -27,27 +34,52 @@ const ItemCount = ({stock, initial, onAdd}) =>{
                
     }
 
+    useEffect(() => {
+        if(stock==0){
+            setVariant("danger")
+        }
+    }, [stock])
+
     
 
-    return(
-        <form id="counterForm">
-            <br/>           
-            <ButtonGroup >
-            <Button variant="info" value="-" onClick={(e) => handleOnAdd(e)}>-</Button>            
-            <InputGroup.Text > - <strong>{contador}</strong> - </InputGroup.Text>
-            <Button variant="info" value="+" onClick={(e) => handleOnAdd(e)}>+</Button>
-            </ButtonGroup>
-            <Card.Body>
-            <Button
-                variant="outline-info"
-                name="submit"
-                type="sumbit" 
-                onClick={(e)=>{handleOnSubmit(e)}} 
-                block               
-                >Agregar
-            </Button>            
-            </Card.Body>           
-        </form>
+    return (
+      <form id='counterForm'>
+        <br />
+        <ButtonGroup>
+          <Button variant={variant} value='-' onClick={(e) => handleCounter(e)}>
+            -
+          </Button>
+          <InputGroup.Text>
+            {" "}- <strong>{contador}</strong> -{" "}
+          </InputGroup.Text>
+          <Button variant={variant} value='+' onClick={(e) => handleCounter(e)}>
+            +
+          </Button>
+        </ButtonGroup>
+        <Form.Group controlId='formBasicRange'>
+        <RangeSlider
+            variant={variant}
+            className="p-3"
+            value={contador}
+            name='range'
+            min={initial}
+            max={stock}
+            onChange={changeEvent => setContador(changeEvent.target.value)}
+          />         
+        </Form.Group>
+        <Card.Body className="p-1">
+        <Button
+            variant={'outline-'+variant}
+            name='submit'
+            type='sumbit'
+            onClick={(e) => {
+              handleOnSubmit(e);
+            }}
+            block>
+            Agregar
+          </Button>
+        </Card.Body>
+      </form>
     );
 }
 
